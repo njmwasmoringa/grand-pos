b2b716a2f5b3aef95503cf7960ec0abfadd5398e
mwangi julius <julius.mwangi@moringaschool.com>
2022-03-22 02:01:29 +0300

    updated the service to query from local db

--- js/service.js
+++ js/service.js
@@ -1,9 +1,9 @@
 async function authenticate(username, password){
-    return await fetch('http://localhost:3000/users').then(resp=>resp.json()).then(users=>{
-        return users.find(user => user.username == username && user.password == password);
+    return await fetch('../db.json').then(resp=>resp.json()).then(json=>{
+        return json.users.find(user => user.username == username && user.password == password);
     });
 }
 
 async function products(){
-    return await fetch('http://localhost:3000/products').then(resp => resp.json());
+    return await fetch('../db.json').then(resp => resp.json()).then(json=>json.products);
 }
